adbc604b93321e962757f594dab141e7cfcb4261
SunnyRay <you@example.com>
2025-01-04 21:14:43 +0800

    solved flexbox issues

--- components/shared/item-list/ItemList.tsx
+++ components/shared/item-list/ItemList.tsx
@@ -14,19 +14,16 @@ const ItemList = ({ children, title, Action }: Props) => {
   const { isActive } = useConversation();
   return (
     <Card
-      className={cn(
-        "hidden min-h-[calc(100vh-110px)] w-full lg:flex-none lg:w-80 p-2",
-        {
-          block: !isActive,
-          "lg:block": isActive,
-        }
-      )}
+      className={cn("hidden h-full w-full lg:flex-none lg:w-80 p-2", {
+        block: !isActive,
+        "lg:block": isActive,
+      })}
     >
       <div className="mb-4 flex items-center justify-between">
         <h1 className="text-2xl font-semibold tracking-tight">{title}</h1>
         {Action ? Action : null}
       </div>
-      <div className="w-full h-full flex flex-col items-center justify-start gap-2">
+      <div className="w-full flex-1 flex flex-col items-center justify-start gap-2">
         {children}
       </div>
     </Card>
